62631df452d6ddfed16b505ff7a041cd7e0cebdb
swtwfrs <partyy@iu-9.info>
2018-01-28 20:43:22 +0400

    One-to-one relation for Book and Publisher classes
    Added Publisher class
    Replaced string property publisher in Book class with One-to-One relation with Publisher class
    Added PublisherRepository
    Updated bootstrap to create and link books with publishers

--- src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.model.Author;
 import guru.springframework.spring5webapp.model.Book;
+import guru.springframework.spring5webapp.model.Publisher;
 import guru.springframework.spring5webapp.model.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.model.repositories.BookRepository;
+import guru.springframework.spring5webapp.model.repositories.PublisherRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -13,10 +15,12 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
-    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -25,9 +29,14 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
     }
 
     private void initData() {
+        Publisher harperCollinsPublisher = new Publisher("Harper Collins", "Somewhere in USA");
+        publisherRepository.save(harperCollinsPublisher);
+        Publisher worxPublisher = new Publisher("Workx", "Some other place");
+        publisherRepository.save(worxPublisher);
         //Eric
         Author eric = new Author("Eric", "Evans");
-        Book dddBook = new Book("Domain Driven Development", "1234", "Harper Collins");
+        Book dddBook = new Book("Domain Driven Development", "1234");
+        dddBook.setPublisher(harperCollinsPublisher);
         eric.getBooks().add(dddBook);
         dddBook.getAuthors().add(eric);
 
@@ -36,9 +45,10 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
         //Rod
         Author rod = new Author("Rod", "Johnson");
-        Book noEjbBook = new Book("J2EE Development without EJB", "23444", "Worx");
-        rod.getBooks().add(noEjbBook);
+        Book noEjbBook = new Book("J2EE Development without EJB", "23444");
+        noEjbBook.setPublisher(worxPublisher);
         noEjbBook.getAuthors().add(rod);
+        rod.getBooks().add(noEjbBook);
 
         authorRepository.save(rod);
         bookRepository.save(noEjbBook);
--- src/main/java/guru/springframework/spring5webapp/model/Book.java
+++ src/main/java/guru/springframework/spring5webapp/model/Book.java
@@ -12,8 +12,10 @@ public class Book {
     private Long id;
     private String title;
     private String isbn;
-    private String publisher;
 
+    @OneToOne
+//    @Column(name = "publisher_id")
+    private Publisher publisher;
     @ManyToMany
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
     inverseJoinColumns = @JoinColumn(name = "author_id"))
@@ -22,13 +24,12 @@ public class Book {
     public Book() {
     }
 
-    public Book(String title, String isbn, String publisher) {
+    public Book(String title, String isbn) {
         this.title = title;
         this.isbn = isbn;
-        this.publisher = publisher;
     }
 
-    public Book(String title, String isbn, String publisher, Set<Author> authors) {
+    public Book(String title, String isbn, Publisher publisher, Set<Author> authors) {
         this.title = title;
         this.isbn = isbn;
         this.publisher = publisher;
@@ -59,20 +60,20 @@ public class Book {
         this.isbn = isbn;
     }
 
-    public String getPublisher() {
-        return publisher;
-    }
-
-    public void setPublisher(String publisher) {
-        this.publisher = publisher;
-    }
-
     public Set<Author> getAuthors() {
         return authors;
     }
 
     public void setAuthors(Set<Author> authors) {
         this.authors = authors;
+    }
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
     }
 
     @Override
@@ -96,7 +97,6 @@ public class Book {
                 "id=" + id +
                 ", title='" + title + '\'' +
                 ", isbn='" + isbn + '\'' +
-                ", publisher='" + publisher + '\'' +
                 ", authors=" + authors +
                 '}';
     }
